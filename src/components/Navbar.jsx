0f66a3a2b06e021e4c46d094930e01f47dfa4362
Azaharul Islam <Azaharulislam171@gmail.com>
2026-01-20 09:15:19 -0500

    Added mobile Navbar utilities

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 import styles from '../styles/Navbar.module.css'
-
+import { Menu, X } from 'lucide-react'; 
+import {useState} from 'react';
 function Navbar(){
+    const [isOpen, setIsOpen] = useState(false);
+    function toggle(){
+        setIsOpen(!isOpen);
+    }
     return(
         <>
         <nav className={styles.navbar}>
@@ -13,6 +18,24 @@ function Navbar(){
                 <li className="caption1">  <a href="mailto:azaharulislam171@gmail.com"> CONTACT </a>   </li>
             </ul>
         </nav>
+        <nav className={styles.mobile_navbar}>
+            <Link to='/'>  <p className="display"> O.B. </p> </Link>
+           {isOpen? <X onClick={toggle}/>: <Menu onClick={toggle}/>}
+           
+        </nav>
+        {/*  This is a classical and gate example. Both parts need to be true to work*/}
+        {isOpen && (
+            <div> 
+                <ul  className={styles.mobile_ul}> 
+                    <li className="caption1" onClick="toggle"> <Link to='/gallery'> GALLERY </Link>  </li>
+                    <li className="caption1" onClick="toggle"> <Link to='/about'> ABOUT </Link>  </li>
+                    <li className="caption1">  <a href="mailto:azaharulislam171@gmail.com"> CONTACT </a>   </li>
+                </ul>
+         </div>)}
+
+
+        
+
         <hr className='divider'/>
         </>
     )
